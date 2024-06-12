0846700908c0df123ffef31af937b61a40db1259
Mercymaryantony <mercyantony1112@gmail.com>
2024-06-12 14:09:37 +0530

    DELETE API DONE

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const {recipemodel} = require("./model/recipe")
 app.use(express.json())
 mongoose.connect("mongodb+srv://mercy1112:mercy1112@cluster0.8x8j3ya.mongodb.net/recipeDB?retryWrites=true&w=majority&appName=Cluster0")
 
-app.get("/view",(req,res)=>{
+app.post("/view",(req,res)=>{
     recipemodel.find().then(
         (data)=>{
             res.json(data)
@@ -20,6 +20,26 @@ app.post("/add",(req,res)=>{
     console.log(recipe)
     recipe.save()
     res.send({"status":"ADD"})
+})
+app.post("/search",(req,res)=>{
+    let input = req.body
+    recipemodel.find(input).then((data)=>{
+        res.json(data)
+    }).catch((error)=>{
+        res.send("error")
+    })
+})
+app.post("/delete",(req,res)=>{
+    let input = req.body
+    recipemodel.findByIdAndDelete(input._id).then(
+        (response)=>{
+            res.json({"status":"deleted"})
+        }
+    ).catch((error)=>{
+        res.json({"status":"error"})
+    })
+
+    
 })
 app.listen(8080,()=>{
     console.log("server started")
